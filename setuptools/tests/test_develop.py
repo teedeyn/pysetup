059377b739f443043fb8f0bba073630382d0b4aa
Earnbit Dashboard <96800091+teedeyn@users.noreply.github.com>
2022-03-19 00:29:21 -0400

    Initialize develop-nspkg

---
+++ setuptools/tests/test_develop.py
@@ -0,0 +1,203 @@
+"""develop tests
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+import site
+import sys
+import io
+import textwrap
+import subprocess
+
+from setuptools.extern import six
+
+import pytest
+
+from setuptools.command.develop import develop
+from setuptools.dist import Distribution
+from . import contexts
+
+SETUP_PY = """\
+from setuptools import setup
+
+setup(name='foo',
+    packages=['foo'],
+    use_2to3=True,
+)
+"""
+
+INIT_PY = """print "foo"
+"""
+
+
+@pytest.yield_fixture
+def temp_user(monkeypatch):
+    with contexts.tempdir() as user_base:
+        with contexts.tempdir() as user_site:
+            monkeypatch.setattr('site.USER_BASE', user_base)
+            monkeypatch.setattr('site.USER_SITE', user_site)
+            yield
+
+
+@pytest.yield_fixture
+def test_env(tmpdir, temp_user):
+    target = tmpdir
+    foo = target.mkdir('foo')
+    setup = target / 'setup.py'
+    if setup.isfile():
+        raise ValueError(dir(target))
+    with setup.open('w') as f:
+        f.write(SETUP_PY)
+    init = foo / '__init__.py'
+    with init.open('w') as f:
+        f.write(INIT_PY)
+    with target.as_cwd():
+        yield target
+
+
+class TestDevelop:
+    in_virtualenv = hasattr(sys, 'real_prefix')
+    in_venv = hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix
+
+    @pytest.mark.skipif(in_virtualenv or in_venv,
+        reason="Cannot run when invoked in a virtualenv or venv")
+    def test_2to3_user_mode(self, test_env):
+        settings = dict(
+            name='foo',
+            packages=['foo'],
+            use_2to3=True,
+            version='0.0',
+        )
+        dist = Distribution(settings)
+        dist.script_name = 'setup.py'
+        cmd = develop(dist)
+        cmd.user = 1
+        cmd.ensure_finalized()
+        cmd.install_dir = site.USER_SITE
+        cmd.user = 1
+        with contexts.quiet():
+            cmd.run()
+
+        # let's see if we got our egg link at the right place
+        content = os.listdir(site.USER_SITE)
+        content.sort()
+        assert content == ['easy-install.pth', 'foo.egg-link']
+
+        # Check that we are using the right code.
+        fn = os.path.join(site.USER_SITE, 'foo.egg-link')
+        with io.open(fn) as egg_link_file:
+            path = egg_link_file.read().split()[0].strip()
+        fn = os.path.join(path, 'foo', '__init__.py')
+        with io.open(fn) as init_file:
+            init = init_file.read().strip()
+
+        expected = 'print("foo")' if six.PY3 else 'print "foo"'
+        assert init == expected
+
+    def test_console_scripts(self, tmpdir):
+        """
+        Test that console scripts are installed and that they reference
+        only the project by name and not the current version.
+        """
+        pytest.skip("TODO: needs a fixture to cause 'develop' "
+            "to be invoked without mutating environment.")
+        settings = dict(
+            name='foo',
+            packages=['foo'],
+            version='0.0',
+            entry_points={
+                'console_scripts': [
+                    'foocmd = foo:foo',
+                ],
+            },
+        )
+        dist = Distribution(settings)
+        dist.script_name = 'setup.py'
+        cmd = develop(dist)
+        cmd.ensure_finalized()
+        cmd.install_dir = tmpdir
+        cmd.run()
+        # assert '0.0' not in foocmd_text
+
+
+class TestNamespaces:
+    @staticmethod
+    def build_namespace_package(tmpdir, name):
+        src_dir = tmpdir / name
+        src_dir.mkdir()
+        setup_py = src_dir / 'setup.py'
+        namespace, sep, rest = name.partition('.')
+        script = textwrap.dedent("""
+            import setuptools
+            setuptools.setup(
+                name={name!r},
+                version="1.0",
+                namespace_packages=[{namespace!r}],
+                packages=[{namespace!r}],
+            )
+            """).format(**locals())
+        setup_py.write_text(script, encoding='utf-8')
+        ns_pkg_dir = src_dir / namespace
+        ns_pkg_dir.mkdir()
+        #pkg_init = ns_pkg_dir / '__init__.py'
+        #tmpl = '__import__("pkg_resources").declare_namespace({namespace!r})'
+        #decl = tmpl.format(**locals())
+        #pkg_init.write_text(decl, encoding='utf-8')
+        pkg_mod = ns_pkg_dir / (rest + '.py')
+        some_functionality = 'name = {rest!r}'.format(**locals())
+        pkg_mod.write_text(some_functionality, encoding='utf-8')
+        return src_dir
+
+    @staticmethod
+    def make_site_dir(target):
+        """
+        Add a sitecustomize.py module in target to cause
+        target to be added to site dirs such that .pth files
+        are processed there.
+        """
+        sc = target / 'sitecustomize.py'
+        target_str = str(target)
+        tmpl = '__import__("site").addsitedir({target_str!r})'
+        sc.write_text(tmpl.format(**locals()), encoding='utf-8')
+
+    @staticmethod
+    def install_develop(src_dir, target):
+
+        develop_cmd = [
+            sys.executable,
+            'setup.py',
+            'develop',
+            '--install-dir', str(target),
+        ]
+        env = dict(PYTHONPATH=str(target))
+        with src_dir.as_cwd():
+            subprocess.check_call(develop_cmd, env=env)
+
+    def test_namespace_package_importable(self, tmpdir):
+        """
+        Installing two packages sharing the same namespace, one installed
+        naturally using pip or `--single-version-externally-managed`
+        and the other installed using `develop` should leave the namespace
+        in tact and both packages reachable by import.
+        """
+        pkg_A = self.build_namespace_package(tmpdir, 'myns.pkgA')
+        pkg_B = self.build_namespace_package(tmpdir, 'myns.pkgB')
+        target = tmpdir / 'packages'
+        # use pip to install to the target directory
+        install_cmd = [
+            'pip',
+            'install',
+            str(pkg_A),
+            '-t', str(target),
+        ]
+        subprocess.check_call(install_cmd)
+        self.install_develop(pkg_B, target)
+        self.make_site_dir(target)
+        try_import = [
+            sys.executable,
+            '-c', 'import myns.pkgA; import myns.pkgB',
+        ]
+        env = dict(PYTHONPATH=str(target))
+        subprocess.check_call(try_import, env=env)
+
